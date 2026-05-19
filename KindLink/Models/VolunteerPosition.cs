7271f1e7bdb30cf70c9daee13e899b66fba8dfee
NaomiSoubhia <soubhiadoinaomi@gmail.com>
2026-05-19 15:05:43 -0400

    Creating VolunteerPosition model

---
+++ KindLink/Models/VolunteerPosition.cs
@@ -0,0 +1,17 @@
+namespace KindLink.Models;
+
+public class VolunteerPosition
+{
+    public int VolunteerPositionId { get; set; } // Primary Key
+    
+    //Atributes
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public DateTime EventDate { get; set; }
+
+    public string Location { get; set; }
+    
+    public int OrganizationId { get; set; } // Foreign Key
+}
