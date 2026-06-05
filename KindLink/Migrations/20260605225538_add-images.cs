a4c285254b889548a17562524a6c7a1b5c694072
NaomiSoubhia <soubhiadoinaomi@gmail.com>
2026-06-05 19:05:59 -0400

    Adding atribute image to Organization and adjusting delete + create

---
+++ KindLink/Migrations/20260605225538_add-images.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KindLink.Migrations
+{
+    /// <inheritdoc />
+    public partial class addimages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Organization",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Organization");
+        }
+    }
+}
