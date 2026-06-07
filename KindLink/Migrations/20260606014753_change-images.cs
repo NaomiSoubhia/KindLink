ae35b3fe765acce029ce578c55db57307465d9cc
NaomiSoubhia <soubhiadoinaomi@gmail.com>
2026-06-06 22:29:46 -0400

    path adjust mac ios rider

---
+++ KindLink/Migrations/20260606014753_change-images.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KindLink.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeimages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Organization");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "VolunteerPosition",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "VolunteerPosition");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Organization",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
