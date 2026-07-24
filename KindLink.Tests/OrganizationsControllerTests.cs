94e6421f7aa4d7b9a2fec47223bcad2d2917c3ff
NaomiSoubhia <soubhiadoinaomi@gmail.com>
2026-07-23 23:09:46 -0400

    Adding initial test class for OrganizationsController

---
+++ KindLink.Tests/OrganizationsControllerTests.cs
@@ -0,0 +1,16 @@
+﻿using KindLink.Controllers;
+using KindLink.Data;
+using KindLink.Models;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KindLink.Tests
+{
+    [TestClass]
+    public class OrganizationsControllerTests
+    {
+
+    }
+}
---
+++ KindLink/Migrations/20260707234639_google-auth.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KindLink.Migrations
+{
+    /// <inheritdoc />
+    public partial class googleauth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
