1b7a5f2191af724943059eeb40261cca7e9ef881
NaomiSoubhia <soubhiadoinaomi@gmail.com>
2026-07-24 16:36:27 -0400

    Implement GetDatabaseContext with data

--- KindLink.Tests/OrganizationsControllerTests.cs
+++ KindLink.Tests/OrganizationsControllerTests.cs
@@ -11,6 +11,50 @@ namespace KindLink.Tests
     [TestClass]
     public class OrganizationsControllerTests
     {
+        private ApplicationDbContext GetDatabaseContext()
+        {
+            var options =
+                new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                    .Options;
+
+            var context = new ApplicationDbContext(options);
+
+            context.Organization.AddRange(
+
+                new Organization
+                {
+                    OrganizationId = 1,
+                    Name = "Georgian Mall",
+                    Email = "georgianmall@test.ca",
+                    PhoneNumber = "1111111111",
+                    Address = "Barrie"
+                },
+
+                new Organization
+                {
+                    OrganizationId = 2,
+                    Name = "Food Bank",
+                    Email = "food@test.ca",
+                    PhoneNumber = "2222222222",
+                    Address = "Barrie"
+                },
+
+                new Organization
+                {
+                    OrganizationId = 3,
+                    Name = "Hospital",
+                    Email = "hospital@test.ca",
+                    PhoneNumber = "3333333333",
+                    Address = "Toronto"
+                }
+
+            );
+
+            context.SaveChanges();
+
+            return context;
+        }
 
     }
 }
