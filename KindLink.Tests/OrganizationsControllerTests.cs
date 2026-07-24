fa09b6368ba44273d9d3eeb57651b4ab63f81307
NaomiSoubhia <soubhiadoinaomi@gmail.com>
2026-07-24 19:50:34 -0400

    Test 6: add POST Edit invalid model test returning View

--- KindLink.Tests/OrganizationsControllerTests.cs
+++ KindLink.Tests/OrganizationsControllerTests.cs
@@ -56,97 +56,97 @@ namespace KindLink.Tests
             return context;
         }
         [TestMethod]
-        public void Edit_Get_ValidId_ReturnsViewResult()
+        public void Edit_Get_ValidId_ReturnsViewResult() 
         {
             // Arrange
             var context = GetDatabaseContext();
-
-            var controller =
-                new OrganizationsController(context);
-
+            var controller = new OrganizationsController(context);
+    
             // Act
             var result = controller.Edit(1);
-
+    
             // Assert
-            Assert.IsInstanceOfType(
-                result,
-                typeof(ViewResult));
+            Assert.IsInstanceOfType(result, typeof(ViewResult)); 
         }
         
         [TestMethod]
         public void Edit_Get_InvalidId_ReturnsNotFound()
         {
+            // Arrange
             var context = GetDatabaseContext();
+            var controller = new OrganizationsController(context);
 
-            var controller =
-                new OrganizationsController(context);
-
+            // Act
             var result = controller.Edit(99);
 
-            Assert.IsInstanceOfType(
-                result,
-                typeof(NotFoundResult));
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
         }
+        
         [TestMethod]
         public void Edit_Post_ValidModel_ReturnsRedirect()
         {
+            // Arrange
             var context = GetDatabaseContext();
+            var controller = new OrganizationsController(context);
+            var organization = context.Organization.First();
 
-            var controller =
-                new OrganizationsController(context);
+            // Act
+            var result = controller.Edit(organization);
 
-            var organization =
-                context.Organization.First();
-
-            var result =
-                controller.Edit(organization);
-
-            Assert.IsInstanceOfType(
-                result,
-                typeof(RedirectToActionResult));
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
         }
+        
         [TestMethod]
-        public void Edit_Post_ValidModel_RedirectsToIndex()
+        public void Edit_Post_ValidModel_RedirectsToIndex() 
         {
+            // Arrange
             var context = GetDatabaseContext();
-
-            var controller =
-                new OrganizationsController(context);
-
-            var organization =
-                context.Organization.First();
-
-            var result =
-                controller.Edit(organization);
-
-            var redirect =
-                result as RedirectToActionResult;
-
-            Assert.AreEqual(
-                "Index",
-                redirect.ActionName);
+            var controller = new OrganizationsController(context);
+            var organization = context.Organization.First();
+    
+            // Act
+            var result = controller.Edit(organization);
+    
+            // Assert
+            var redirect = result as RedirectToActionResult;
+            Assert.AreEqual("Index", redirect.ActionName); 
         }
+        
         [TestMethod]
         public void Edit_Post_UpdatesDatabase()
         {
+            // Arrange
             var context = GetDatabaseContext();
-
-            var controller =
-                new OrganizationsController(context);
-
-            var organization =
-                context.Organization.Find(1);
-
+            var controller = new OrganizationsController(context);
+    
+            var organization = context.Organization.Find(1);
             organization.Name = "Updated Name";
 
+            // Act
             controller.Edit(organization);
 
-            var updated =
-                context.Organization.Find(1);
+            // Assert
+            var updated = context.Organization.Find(1);
+            Assert.AreEqual("Updated Name", updated.Name);
+        }
+        
+        [TestMethod]
+        public void Edit_Post_InvalidModel_ReturnsView()
+        {
+            // Arrange
+            var context = GetDatabaseContext();
+            var controller = new OrganizationsController(context);
+    
+            controller.ModelState.AddModelError("Name", "Required");
+            var organization = context.Organization.First();
 
-            Assert.AreEqual(
-                "Updated Name",
-                updated.Name);
+            // Act
+            var result = controller.Edit(organization);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
         }
     }
 }
